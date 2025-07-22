b7022534cdbd9fd590b24e1d95481c71000c997e
Benjamin Cardoso <benjamincardoso@MacBook-Pro-de-Benjamin.local>
2025-07-22 22:19:30 +0200

    hjlfbehzj

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,17 +6,41 @@ import { start3D } from './zzzz/scene';
 export default function NotFoundPage() {
   useEffect(() => {
     start3D();
+
+    return () => {
+      const canvas = document.querySelector('canvas');
+      if (canvas) canvas.remove(); // Nettoyage du canvas à la sortie
+    };
   }, []);
-    
+
   return (
     <div
       id="zebi-scene"
       style={{
+        position: 'relative',
         width: '100vw',
         height: '100vh',
-        background: '#000',
+        background: '#F8F9F4',
         overflow: 'hidden',
       }}
-    />
+    >
+      <h1
+        style={{
+          position: 'absolute',
+          top: '10%',
+          left: '50%',
+          transform: 'translateX(-50%)',
+          fontFamily: "'Rubik', sans-serif",
+          fontSize: '4rem',
+          background: 'linear-gradient(90deg, orange, #0033cc)',
+          WebkitBackgroundClip: 'text',
+          WebkitTextFillColor: 'transparent',
+          fontWeight: 'bold',
+          zIndex: 10,
+        }}
+      >
+        Erreur 404
+      </h1>
+    </div>
   );
 }
