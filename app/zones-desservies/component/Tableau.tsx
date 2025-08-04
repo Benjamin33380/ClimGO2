e0a0db4aa69659429ce957c2bf6b43ca3b73286b
Benjamin Cardoso <benjamincardoso@MacBook-Pro-de-Benjamin.local>
2025-08-04 19:20:38 +0200

    aaaaaaaa

--- app/zones-desservies/component/Tableau.tsx
+++ app/zones-desservies/component/Tableau.tsx
@@ -54,14 +54,14 @@ const CitiesTable = () => {
 
   return (
     <div className="max-w-4xl mx-auto p-4 border border-[#b5b7bd] rounded-lg shadow-sm">
-      <h1 className="text-2xl font-bold text-center mb-6">
+      <h3 className="text-2xl font-bold text-center mb-6">
         <a
           href="/zones-desservies"
           className="bg-[#03144a] text-[#F8F9F4] px-4 py-2 rounded shadow hover:opacity-90 transition-opacity"
         >
           Zones d&apos;intervention ClimGO
         </a>
-      </h1>
+      </h3>
 
       <div className="flex flex-wrap gap-2 justify-center">
         {cities.map((city) => (
