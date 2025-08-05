c6f266597a0d929bd7f94c396636bd9def3b564a
Benjamin Cardoso <benjamincardoso@MacBook-Pro-de-Benjamin.local>
2025-08-05 15:46:33 +0200

    lienmarque

--- app/zones-desservies/component/Partenaires.tsx
+++ app/zones-desservies/component/Partenaires.tsx
@@ -12,10 +12,10 @@ export default function Partenaires() {
   );
 }
 const CarouselMarques: React.FC = () => {
-  const images: { src: string; alt: string }[] = [
-    { src: "/DaikinLogo.png", alt: "Daikin" },
-    { src: "/Mitsubishi-Electric-Logo.png", alt: "Mitsubishi Electric" },
-    { src: "/atl.png", alt: "ATL" },
+  const images: { src: string; alt: string; url: string }[] = [
+    { src: "/DaikinLogo.png", alt: "Daikin", url: "https://www.daikin.fr" },
+    { src: "/Mitsubishi-Electric-Logo.png", alt: "Mitsubishi Electric", url: "https://fr.mitsubishielectric.com" },
+    { src: "/atl.png", alt: "ATL", url: "https://www.atlantic.fr" },
   ];
   
   const [index, setIndex] = React.useState(0);
@@ -30,6 +30,11 @@ const CarouselMarques: React.FC = () => {
 
   return (
     <div className="carousel-container relative min-h-[320px] max-w-2xl mx-auto flex items-center justify-center">
+      <a
+        href={images[index].url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Image
           width={1000}
           height={500}
@@ -42,6 +47,7 @@ const CarouselMarques: React.FC = () => {
             w-auto h-auto select-none 
             animate-[zoom8d_4s_ease-in-out_infinite] drop-shadow-2xl"
         />
-      </div>
+      </a>
+    </div>
   );
 };
